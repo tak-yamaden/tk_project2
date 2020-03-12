d023a9638d7aa8d3c1fc33f23172b01a54e1dbcc
yamatak <a_study_in_evergreen@katch.ne.jp>
2020-03-12 21:29:36 +0900

    ch2

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, AboutPAgeView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPAgeView.as_view(), name='about'),
 ]
