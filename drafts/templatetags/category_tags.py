8386b839875d45a4dbe84b64412949d0c50dc41e
yamatak <a_study_in_evergreen@katch.ne.jp>
2020-03-23 05:00:47 +0900

    ch11

--- drafts/templatetags/category_tags.py
+++ drafts/templatetags/category_tags.py
@@ -1,10 +1,18 @@
 from django import template
-from drafts.models import Category
+from django.db.models import Count
+from drafts.models import Category, Draft
 
 register = template.Library()
 
+# @register.inclusion_tag('drafts/tags/category_links.html')
+# def render_category_links():
+#     ctx = {}
+#     ctx['category_list'] = Category.objects.annotate(
+#         count=Count('company'))
+#     return ctx
 @register.inclusion_tag('drafts/tags/category_links.html')
 def render_category_links():
-    return {
-        'category_list': Category.objects.all(),
-    }
+    ctx = {}
+    ctx['draft_list'] = Draft.objects.annotate(
+        count=Count('category'))
+    return ctx
